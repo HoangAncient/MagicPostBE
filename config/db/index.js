208233e68c3624fe40d06941f78f6b357a7d73ad
Hoang Ancient <hoangdinhnho23@gmail.com>
2024-01-03 21:42:27 +0700

    v1.0.0

--- config/db/index.js
+++ config/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const { DB_USER, DB_PASS, DB_LINK } = require("../");
+const { DB_USER, DB_PASS } = require("../");
 
 mongoose.set('strictQuery', true);
 mongoose.connect(
-  `mongodb+srv://${DB_USER}:${DB_PASS + DB_LINK}`
+  `mongodb+srv://${DB_USER}:${DB_PASS}@magicpost.c17ghyd.mongodb.net/?retryWrites=true&w=majority`
 );
 
 //mongodb+srv://MagicPost:12345@magicpost.c17ghyd.mongodb.net/?retryWrites=true&w=majority
